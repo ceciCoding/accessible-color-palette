dd199ab509ca3be3e51bbde8b10c340fceb37baa
Ceci Olivera <ceci.coding@gmail.com>
2023-07-03 23:26:24 +0200

    add typescript to test helpers file

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,19 @@
 export type BgColor = 'white' | 'black'
 
+export type Shade = '100' | '300' | '600' | '700' | '800' | '900'
+
 export interface Validation {
   condition: boolean
   errorMessage: string
+}
+
+export interface LightArg {
+  ratio: number
+  hex: string
+}
+
+export type RatiosObj = Record<Shade, number>
+
+export interface FinalPaletteRatios {
+  [key: string]: number[];
 }
